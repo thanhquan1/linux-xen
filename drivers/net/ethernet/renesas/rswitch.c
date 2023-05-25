cd39c05a387b526eed04f59d47c3536b57acf699
Dat Vo <dat.vo.yb@renesas.com>
2023-05-25 15:33:23 +0700

    net: ethernet: renesas: rswitch: Use parent device for devm allocations
    This change is needed because we are going to rework device
    initialization order in xenback and xenfront. We will allocate chains
    before registering ndev. As at this point ndev->dev is not
    initialized, we can't use it for resource mananger (devm) operations.
    
    But we can use parent device which is already initialized. This is
    perfectly fine, as parent device is a xen pv driver instance, which
    will have (almost) the same lifetime as our ndev.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>
    Signed-off-by: Dat Vo <dat.vo.yb@renesas.com>

--- drivers/net/ethernet/renesas/rswitch.c
+++ drivers/net/ethernet/renesas/rswitch.c
@@ -2491,8 +2491,8 @@ int rswitch_txdmac_init(struct net_device *ndev, struct rswitch_private *priv,
 		if (!rdev->tx_chain)
 			return -EBUSY;
 	} else {
-		rdev->tx_chain = devm_kzalloc(&ndev->dev, sizeof(*rdev->rx_chain),
-					      GFP_KERNEL);
+		rdev->tx_chain = devm_kzalloc(ndev->dev.parent, sizeof(*rdev->rx_chain),
+									GFP_KERNEL);
 		if (!rdev->tx_chain)
 			return -ENOMEM;
 		rdev->tx_chain->index = chain_num;
@@ -2519,7 +2519,7 @@ out_init:
 }
 
 void rswitch_txdmac_free(struct net_device *ndev,
-				struct rswitch_private *priv)
+			struct rswitch_private *priv)
 {
 	struct rswitch_device *rdev = netdev_priv(ndev);
 
@@ -2538,8 +2538,8 @@ int rswitch_rxdmac_init(struct net_device *ndev, struct rswitch_private *priv,
 		if (!rdev->rx_chain)
 			return -EBUSY;
 	} else {
-		rdev->rx_chain = devm_kzalloc(&ndev->dev, sizeof(*rdev->rx_chain),
-					      GFP_KERNEL);
+		rdev->rx_chain = devm_kzalloc(ndev->dev.parent, sizeof(*rdev->rx_chain),
+									GFP_KERNEL);
 		if (!rdev->rx_chain)
 			return -ENOMEM;
 		rdev->rx_chain->index = chain_num;
